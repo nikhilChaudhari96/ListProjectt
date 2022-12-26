96afbcfa18ce5a513099a7bfe2c357fced2f952d
nikhilchaudhari <nik29021996@gmail.com>
2022-12-26 15:42:36 +0530

    first code

---
+++ src/com/pratice/Example.java
@@ -0,0 +1,14 @@
+package com.pratice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example {
+public static void main(String[] args) {
+	List list=new ArrayList();
+	list.add("nikhil");
+	list.add("kranti");
+	list.add("chaudhari");
+	System.out.println(list);
+}
+}
